33dcfee3953c90e8c3e00b73c6b7132e73789b75
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-17 18:26:46 +0300

    Move dashboard into it's own component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import { useEffect, useState } from "react";
+import Dashboard from "./components/Dashboard"
 import diseaseService from "./services/diseaseService";
 
 const App = () => {
   const [diseaseData, setDiseaseData] = useState({});
 
   useEffect(() => {
-    diseaseService.getAll().then(diseaseData => setDiseaseData(diseaseData))
+    diseaseService.getAll().then((diseaseData) => setDiseaseData(diseaseData));
   }, []);
 
   return (
     <div>
-      <div className="flexbox">
-        <div>
-          Total cases: {diseaseData.cases}
-          <br />
-          Today cases: {diseaseData.todayCases}
-          <br />
-          Active cases: {diseaseData.active}
-        </div>
-        <div>
-          Total deaths: {diseaseData.deaths}
-          <br />
-          Today deaths: {diseaseData.todayDeaths}
-          <br />
-          Critical cases: {diseaseData.critical}
-        </div>
-        <div>
-          Total recovered: {diseaseData.recovered}
-          <br />
-          Today recovered: {diseaseData.todayRecovered}
-          <br />
-          Total tests: {diseaseData.tests}
-        </div>
-      </div>
-    </div>
+    {Object.keys(diseaseData).length === 0 ? (
+      <div>loading</div>
+    )
+      :
+      (
+        <Dashboard data={diseaseData}/>
+    )
+    }
+</div>
   );
 };
 
