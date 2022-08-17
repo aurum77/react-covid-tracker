8a1a31328c9fba29d25d411594fc397b4aa324a1
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-17 19:37:30 +0300

    Add color to cards in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,71 +1,59 @@
-import Container from "react-bootstrap/Container";
 import CardGroup from "react-bootstrap/CardGroup";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
-const Dashboard = ({data}) => {
+const Dashboard = ({ data }) => {
   return (
     <>
-      <Container>
-        <CardGroup className="mt-2">
-          <Card className="mb-2">
+      <Container className="mt-4">
+        <CardGroup>
+          <Card className="mb-2" bg="danger" text="light">
             <Card.Body>
               <Card.Title>Total cases:</Card.Title>
               <Card.Text>{data.cases.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="light" text="dark">
             <Card.Body>
               <Card.Title>New cases today :</Card.Title>
-              <Card.Text>
-                {data.todayCases.toLocaleString("en-US")}
-              </Card.Text>
+              <Card.Text>{data.todayCases.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="primary" text="light">
             <Card.Body>
               <Card.Title>Active cases:</Card.Title>
-              <Card.Text>
-                {data.active.toLocaleString("en-US")}
-              </Card.Text>
+              <Card.Text>{data.active.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
         </CardGroup>
         <CardGroup>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="dark" text="white">
             <Card.Body>
               <Card.Title>Total deaths:</Card.Title>
-              <Card.Text>
-                {data.deaths.toLocaleString("en-US")}
-              </Card.Text>
+              <Card.Text>{data.deaths.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="secondary" text="white">
             <Card.Body>
               <Card.Title>New deaths today:</Card.Title>
-              <Card.Text>
-                {data.todayDeaths.toLocaleString("en-US")}
-              </Card.Text>
+              <Card.Text>{data.todayDeaths.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="warning">
             <Card.Body>
               <Card.Title>Critical cases:</Card.Title>
-              <Card.Text>
-                {data.critical.toLocaleString("en-US")}
-              </Card.Text>
+              <Card.Text>{data.critical.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
         </CardGroup>
         <CardGroup>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="success" text="light">
             <Card.Body>
               <Card.Title>Total recovered:</Card.Title>
-              <Card.Text>
-                {data.recovered.toLocaleString("en-US")}
-              </Card.Text>
+              <Card.Text>{data.recovered.toLocaleString("en-US")}</Card.Text>
             </Card.Body>
           </Card>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="primary" text="light">
             <Card.Body>
               <Card.Title>New recoveries today:</Card.Title>
               <Card.Text>
@@ -73,7 +61,7 @@ const Dashboard = ({data}) => {
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card className="mb-2">
+          <Card className="mb-2" bg="info">
             <Card.Body>
               <Card.Title>Total tests:</Card.Title>
               <Card.Text>{data.tests.toLocaleString("en-US")}</Card.Text>
