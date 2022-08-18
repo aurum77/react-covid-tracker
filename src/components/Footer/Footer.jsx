2a999f0a929262a1c4d12b163f1eb5286a5fd287
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-18 15:48:22 +0300

    Add custom Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <footer>
+        <div className="footerText">
+          © 2022 Made with 💗 by aurum77
+        </div>
+      </footer>
+    </div>
+  );
+};
