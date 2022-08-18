86869da934fe041a1035e6fff215075ca24779ef
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-19 00:27:09 +0300

    Media query adjustments and formatting

--- src/components/Flex/Flex.jsx
+++ src/components/Flex/Flex.jsx
@@ -1,7 +1,5 @@
 import "./Flex.css";
 
-export const Flex= (props) => {
-  return <div className="flex">
-    {props.children}
-    </div>;
+export const Flex = (props) => {
+  return <div className="flex">{props.children}</div>;
 };
