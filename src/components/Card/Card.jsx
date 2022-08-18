eb0f31b7f46edb6687ecbe3921a239d6aadaf61b
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-18 23:46:55 +0300

    Add prop to change background color and text color

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,8 @@
 import "./Card.css";
 
-export const Card = ({ header, body }) => {
+export const Card = ({ background, color, header, body }) => {
   return (
-    <div className="card">
+    <div className="card" style={{ background: background, color: color }}>
       <div className="container">
         <h2>{header}</h2>
         <h1>{body}</h1>
