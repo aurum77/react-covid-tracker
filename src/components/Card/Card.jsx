a4ce931379daad5985bba8638a6a4eadea26edcf
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-08-26 01:31:05 +0300

    Use BEM naming and adjust styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,8 @@ import "./Card.css";
 export const Card = ({ background, color, header, body }) => {
   return (
     <div className="card" style={{ background: background, color: color }}>
-      <div className="container">
-        <h2>{header}</h2>
-        <h1>{body}</h1>
-      </div>
+      <div className="card__header">{header}</div>
+      <div className="card__body">{body}</div>
     </div>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,7 @@ export const Footer = () => {
   return (
     <div className="footer">
       <footer>
-        <div className="footerText">
-          © 2022 Made with 💗 by aurum77
-        </div>
+        <div className="footer__text">© 2022 Made with 💗 by aurum77</div>
       </footer>
     </div>
   );
