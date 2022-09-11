991e821b641318b623d2df52e94e5bc6da4d4561
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-12 01:00:11 +0300

    feat: implement routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { useEffect, useState } from "react";
-import { Footer } from "./components/Footer";
-import { Dashboard } from "./components";
-import * as diseaseService from "./services/diseaseService";
+import { Footer, Header } from "./components";
+import { NotesDashboardPage, NotFoundPage } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [diseaseData, setDiseaseData] = useState({});
-
-  useEffect(() => {
-    diseaseService.getAll().then((diseaseData) => setDiseaseData(diseaseData));
-  }, []);
-
   return (
     <div>
-      {Object.keys(diseaseData).length === 0 ? (
-        <div>Fetching data from API</div>
-      ) : (
-        <Dashboard data={diseaseData} />
-      )}
-      <div>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<NotesDashboardPage />} />
+        </Routes>
         <Footer />
-      </div>
+      </Router>
     </div>
   );
 };
