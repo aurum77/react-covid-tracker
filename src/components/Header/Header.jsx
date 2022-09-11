ef90017ae154f696cc5cc279d222ca1b699a1eed
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-12 00:54:33 +0300

    feat: add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export const Header = () => {
+  return(
+    <div className="header">
+      <img className="header__icon" src="./logo192.png" />
+      <div className="header__text">react-covid-tracker</div>
+    </div>
+  )
+}
