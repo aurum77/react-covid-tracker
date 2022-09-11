393ecb112ea504b3890f8ac4ef07c7d215ecb80f
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-12 00:54:46 +0300

    feat: add notfound component

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import "./NotFound.css";
+
+export const NotFound = () => {
+  return(
+    <div className="notfound">
+      <img src="https://http.cat/404" alt="a cat hiding under some paper"/>
+    </div>
+  )
+}
