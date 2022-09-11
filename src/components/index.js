a7eed0644bbe2fbd9f7a3adfa3d62d3777c3f1ed
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-12 00:59:25 +0300

    fix: add import barreling to components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
-export { Dashboard } from "./Dashboard"
+export { Card } from "./Card";
+export { Footer } from "./Footer";
+export { Dashboard } from "./Dashboard";
+export { Header } from "./Header";
+export { NotFound } from "./NotFound";
