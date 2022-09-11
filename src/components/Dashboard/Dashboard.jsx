a7c0692631384fce5e1d32f7205fe66a577952c3
aurum77 <58880138+aurum77@users.noreply.github.com>
2022-09-11 23:42:30 +0300

    refactor: put dashboard in its own folder

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,54 @@
+import "./Dashboard.css";
+import { Card } from "../Card";
+
+export const Dashboard = ({ data }) => {
+  return (
+    <div className="dashboard">
+      <Card
+        modifier="card--turquoise"
+        header="Total cases:"
+        body={data.cases.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--blue"
+        header="New cases today:"
+        body={data.todayCases.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--lightblue"
+        header="Active cases:"
+        body={data.active.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--black"
+        header="Total deaths:"
+        body={data.deaths.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--gray"
+        header="New deaths today:"
+        body={data.todayDeaths.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--red"
+        header="Critical cases:"
+        body={data.critical.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--lightgreen"
+        header="Total recovered:"
+        body={data.recovered.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--lightblue"
+        header="New recoveries today:"
+        body={data.todayRecovered.toLocaleString("en-US")}
+      />
+      <Card
+        modifier="card--tests"
+        header="Total tests:"
+        body={data.tests.toLocaleString("en-US")}
+      />
+    </div>
+  );
+};
